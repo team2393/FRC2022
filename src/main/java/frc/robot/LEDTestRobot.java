7d67cee01794ff409074cd13aa1712bcaca6abb5
Kay <kay.kasemir@gmail.com>
2022-01-11 12:50:48 -0500

    LEDTestRobot

---
+++ src/main/java/frc/robot/LEDTestRobot.java
@@ -0,0 +1,35 @@
+// Copyright (c) Team 2393, FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+package frc.robot;
+
+import edu.wpi.first.wpilibj.DigitalOutput;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.TimedRobot;
+
+/** Very simple robot that blinks LED */
+public class LEDTestRobot extends TimedRobot
+{
+    private final DigitalOutput led = new DigitalOutput(1);
+
+    @Override
+    public void robotInit()
+    {
+        // Print something that allows us to see on the roboRio what's been uploaded
+        System.out.println("***** Team 2393 LED Test *****");
+    }
+
+    @Override
+    public void teleopPeriodic()
+    {
+        // In teleop, manually turn LED on/off via the 'USER' button
+        led.set(RobotController.getUserButton());
+    }
+
+    @Override
+    public void autonomousPeriodic()
+    {
+        // In autonomous, toggle LED on/off every half second
+        led.set((System.currentTimeMillis() / 500) % 2 == 0);
+    }
+}
