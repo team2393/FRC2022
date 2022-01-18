950da4b80e533d687ddea6a387d6d519384ce2f3
Kay <kay.kasemir@gmail.com>
2022-01-18 12:23:25 -0500

    Test Rev pwr and pneumatic hubs

--- src/main/java/frc/robot/PneumaticHubTestRobot.java
+++ src/main/java/frc/robot/PneumaticHubTestRobot.java
@@ -7,12 +7,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.PowerDistribution;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.TimedRobot;
 
-/** Very simple robot for testing the new REV Robotics Pneumatic Hub */
+/** Very simple robot for testing the new REV Robotics Pneumatic and power Hub */
 public class PneumaticHubTestRobot extends TimedRobot
 {
+    private PowerDistribution power = new PowerDistribution();
+
     // private final PneumaticHub hub = new PneumaticHub();
     private List<Solenoid> solenoids = new ArrayList<>();
 
@@ -21,6 +24,8 @@ public class PneumaticHubTestRobot extends TimedRobot
     {
         // Print something that allows us to see on the roboRio what's been uploaded
         System.out.println("***** Team 2393 Pneumatic Hub Test *****");
+
+        System.out.println("Power distribution board channels: " + power.getNumChannels());
 
         // Create one solenoid for each channel
         for (int ch=0; ch<16; ++ch)
@@ -33,5 +38,8 @@ public class PneumaticHubTestRobot extends TimedRobot
         // Every second, toggle another of the channels 0..15
         final int ch = (int) (System.currentTimeMillis() / 1000) % 16;
         solenoids.get(ch).toggle();
+
+        // Every 10 seconds, toggle the switchable channel on the Rev power hub
+        power.setSwitchableChannel((System.currentTimeMillis() / 10000) % 2 == 1);
     }
 }
