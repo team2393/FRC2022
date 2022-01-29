fe00ef11d8750607626a16391b1d78871dc34aa2
Kay <kay.kasemir@gmail.com>
2022-01-29 12:18:33 -0500

    Add ball test robot

---
+++ src/main/java/frc/robot/BallDetectorTestRobot.java
@@ -0,0 +1,38 @@
+// Copyright (c) Team 2393, FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+package frc.robot;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/** Very simple robot that tests fiber optic ball detector */
+public class BallDetectorTestRobot extends TimedRobot
+{
+    /** Sensor, connected to DIO on RoboRIO
+     * 
+     *  Detector black   = Digital Input,
+     *           brown   = 12V
+     *           blue    = Gnd
+     */
+    private final DigitalInput fiberopticswtich = new DigitalInput(7);
+        
+    @Override
+    public void robotInit()
+    {
+        // Print something that allows us to see on the roboRio what's been uploaded
+        System.out.println("***** Team 2393 Ball Detector Test *****");
+    }
+
+    @Override
+    public void robotPeriodic()
+    {
+        // false: Light, no ball
+        // true:  Not connected, no light, ball in path of light
+        if (fiberopticswtich.get())
+            SmartDashboard.putString("Ball Detector", "Ball?");
+        else
+            SmartDashboard.putString("Ball Detector", "No Ball!");
+    }
+}
