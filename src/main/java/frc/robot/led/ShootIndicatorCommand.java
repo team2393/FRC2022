711ca215fdb8038420660fa8321185a61cc3999a
Kay <kay.kasemir@gmail.com>
2022-02-15 18:23:59 -0500

    cleanup

--- src/main/java/frc/robot/led/ShootIndicatorCommand.java
+++ src/main/java/frc/robot/led/ShootIndicatorCommand.java
@@ -1,9 +1,12 @@
+// Copyright (c) Team 2393, FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
 package frc.robot.led;
 
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
+/** Rapid 'shoot' type indicator that runs 3 LEDs up the strip */
 public class ShootIndicatorCommand extends CommandBase
 {
     private LEDStrip strip;
--- src/main/java/frc/robot/led/SpeedIndicatorCommand.java
+++ src/main/java/frc/robot/led/SpeedIndicatorCommand.java
@@ -1,9 +1,13 @@
+// Copyright (c) Team 2393, FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
 package frc.robot.led;
 
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.OperatorInterface;
 
+/** Indicate 'speed' (forward, backward) via LED strip */
 public class SpeedIndicatorCommand extends CommandBase
 {
     private LEDStrip strip;
