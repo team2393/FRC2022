cbd9749d2837d1a10a26f0ca6406ab69fff6ac59
Kay <kay.kasemir@gmail.com>
2022-03-25 18:33:26 -0400

    RotateToHeadingCommand

---
+++ src/main/java/frc/robot/RotateToHeadingTestRobot.java
@@ -0,0 +1,76 @@
+// Copyright (c) Team 2393, FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.drivetrain.DriveByJoystickCommand;
+import frc.robot.drivetrain.Drivetrain;
+import frc.robot.drivetrain.RotateToHeadingCommand;
+
+/** Robot for testing/calibrating rotate-to-heading
+ * 
+ *  [ ] Manually drive
+ *  [ ] Tune parameters
+ *  [ ] Hardcode result in RotateToHeadingCommand and configure timeout
+ */
+public class RotateToHeadingTestRobot extends TimedRobot
+{
+    private final TalonSRX pigeon_carrier = new TalonSRX(RobotMap.LEFT_INTAKE);
+    private final Drivetrain drivetrain = new Drivetrain(pigeon_carrier);
+    private final DriveByJoystickCommand joydrive = new DriveByJoystickCommand(drivetrain);
+    private final RotateToHeadingCommand rotate = new RotateToHeadingCommand(drivetrain, 0.0);
+
+    @Override
+    public void robotInit()
+    {
+        // Print something that allows us to see on the roboRio what's been uploaded
+        System.out.println("***** Team 2393 RotateToHeading Test *****");
+
+        // Allow setting the speed used during auto-no-mouse
+        SmartDashboard.setDefaultNumber("Desired Heading", 0.0);
+
+        SmartDashboard.setDefaultNumber("Tolerance", 1.0);
+        SmartDashboard.setDefaultNumber("P", 0.0);
+        SmartDashboard.setDefaultNumber("I", 0.0);
+        SmartDashboard.setDefaultNumber("D", 0.0);
+        SmartDashboard.setDefaultNumber("max", 0.3);
+    }
+
+    @Override
+    public void robotPeriodic()
+    {
+        // Make commands and subsystems execute
+        CommandScheduler.getInstance().run();
+    }
+
+    @Override
+    public void teleopInit()
+    {
+        drivetrain.reset();
+        joydrive.schedule();
+    }
+
+    @Override
+    public void autonomousInit()
+    {
+        drivetrain.reset();
+        rotate.schedule();
+    }
+
+    @Override
+    public void autonomousPeriodic()
+    {
+        // Allow adjusting PID from dashboard
+        rotate.configure(SmartDashboard.getNumber("Desired Heading", 0.0),
+                         SmartDashboard.getNumber("Tolerance", 0.0),
+                         SmartDashboard.getNumber("P", 0.0),
+                         SmartDashboard.getNumber("I", 0.0),
+                         SmartDashboard.getNumber("D", 0.0),
+                         SmartDashboard.getNumber("max", 0.0));
+    }
+}
