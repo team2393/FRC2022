ea0da4de2890bc378d1e3eafd1be79607e0c0940
Kay Kasemir <kay.kasemir@gmail.com>
2022-03-26 09:49:16 -0400

    Rotatetoheading

--- src/main/java/frc/robot/drivetrain/RotateToHeadingCommand.java
+++ src/main/java/frc/robot/drivetrain/RotateToHeadingCommand.java
@@ -13,8 +13,8 @@ public class RotateToHeadingCommand extends CommandBase
 {
     private final Drivetrain drivetrain;
     private double desired_degrees;
-    private final PIDController pid = new PIDController(0, 0, 0);
-    private double max = 0.5;
+    private final PIDController pid = new PIDController(0.04, 0.05, 0.002);
+    private double max = 0.3;
     private final Timer timer = new Timer();
 
     /** @param drivetrain Drivetrain to control
@@ -51,7 +51,7 @@ public class RotateToHeadingCommand extends CommandBase
     @Override
     public void execute()
     {
-        double rotation = pid.calculate(drivetrain.getHeading(), desired_degrees);
+        double rotation = -pid.calculate(drivetrain.getHeading(), desired_degrees);
         rotation = MathUtil.clamp(rotation, -max, max);
         drivetrain.driveDirect(0, rotation);
     }
@@ -65,6 +65,6 @@ public class RotateToHeadingCommand extends CommandBase
     @Override
     public boolean isFinished()
     {
-        return pid.atSetpoint() || timer.hasElapsed(50.0);
+        return pid.atSetpoint() || timer.hasElapsed(10.0);
     }
 }
